f3bdf1a1ce817bebf5a315d1ace8e2b6077a5899
Hanson Wu <wuhonsun.hs@gmail.com>
2021-06-10 14:14:01 +0800

    add material-ui

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import App from './App';
 
-ReactDOM.render(
-  <App />,
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
+
+render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
   document.getElementById('root'),
 );
